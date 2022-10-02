40319799b8cda2c8b5a48ef38287a7fa51368e35
Halvor <stektpotet@gmail.com>
2022-10-02 03:20:11 +0200

    Add RandomExtensions utility for random operations on collections

---
+++ LDJam51/Assets/Scripts/Extensions/RandomExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace RandomExtensions
+{
+    public static class RandomCollectionExtensions
+    {
+        public static T RandomElement<T>(this T[] arr) => arr[Random.Range(0, arr.Length)];
+        public static T RandomElement<T>(this IList<T> list) => list[Random.Range(0, list.Count)];
+    }
+}
